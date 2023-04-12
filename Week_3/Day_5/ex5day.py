8090942826a7886ebd680f45005d2974630cbc29
Alesya <alesya@gmail.com>
2023-04-12 15:50:35 +0300

    1

--- Week_3/Day_5/ex5day.py
+++ Week_3/Day_5/ex5day.py
@@ -29,3 +29,44 @@ for site in websites:
 # Loading https://www.google.com took 0.35 seconds
 # Loading https://www.ynet.co.il took 0.09 seconds
 # Loading https://www.imdb.com took 0.20 seconds
+
+import random
+class Deck:
+    def __init__(self):
+
+        my_deck = []
+        suit = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        value = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"] 
+        for suit_data in suit:
+            for values in value:
+                my_string = suit_data + " " + values
+                my_deck.append(my_string)
+        self.cards = my_deck
+
+
+
+    def shuffle(self):
+        if len(self.cards) == 52:
+            return random.shuffle(self.cards)
+
+    def deal(self):
+        if len(self.cards) > 0:
+            return self.cards.pop()
+        else:
+            return "Deck is empty"
+           
+       
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"        
+decks = Deck()
+decks.shuffle()
+card1 = decks.deal()
+print(card1)
+
+# Clubs 8
+# Hearts A
