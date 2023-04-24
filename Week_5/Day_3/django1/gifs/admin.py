f989d4a13180883d1420236e9ee3aa70d8d9fd1d
Alesya <alesya@gmail.com>
2023-04-24 21:00:31 +0300

    Day_3_1

--- Week_5/Day_3/django1/gifs/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import GifModel, Category_Model
-
-
-admin.site.register(GifModel)
-admin.site.register(Category_Model)
