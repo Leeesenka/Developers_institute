975f4dc82aac05cc65adc576becabffd1b1ac845
Alesya <alesya@gmail.com>
2023-06-01 09:22:46 +0300

    new

--- Week_10/Day_5/hakaton/alarm/data/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from django.db import models
-
-class Alert(models.Model):
-    date = models.DateField()
-    time = models.TimeField()
-    city = models.CharField(max_length=100)  # Добавлено поле для хранения города
--- Week_10/Day_5/main.js
+++
@@ -1,5 +0,0 @@
-const str = 'hello word';
- 
-const reverse = str.split('').reverse().join('');
-console.log(reverse);
- 
