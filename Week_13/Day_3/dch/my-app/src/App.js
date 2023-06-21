e12bc86acb2fc87c8005277c700ddcb7f6a34059
Alesya <alesya@gmail.com>
2023-06-21 16:58:14 +0300

    day_3_dch

---
+++ Week_13/Day_3/dch/my-app/src/App.js
@@ -0,0 +1,13 @@
+import RoboFriends from './components/RoboFriends';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <RoboFriends/>
+    </div>
+  );
+}
+
+export default App;
+
